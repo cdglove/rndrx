489ecf53e567928d01b2d160ceb16bac10fd3b62
Chris Glover <chrisglover@google.com>
2022-10-31 08:19:43 +0000

    Build script cleanup

--- cmake/models.cmake
+++ cmake/models.cmake
@@ -11,14 +11,33 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-function(add_models TARGET SOURCES)
-  cmake_parse_arguments(
-          ADD_MODELS
-          "TARGET"
-          "SOURCES"
-          ${ARGN})
-  foreach(FILE IN ${ADD_MODELS_SOURCES})
-    message(${FILE})
-  endforeach()
+function(_compile_model MODEL_FILE_IN MODEL_FILE_OUT COMPILER_FLAGS)
+  add_custom_command(OUTPUT "${MODEL_FILE_OUT}"
+                     MAIN_DEPENDENCY ${MODEL_FILE_IN}
+                     COMMAND ${CMAKE_COMMAND} -E copy ${MODEL_FILE_IN} ${MODEL_FILE_OUT}
+                     COMMENT "Building model ${MODEL_FILE_OUT} from ${MODEL_FILE_IN}"
+                     VERBATIM)
 endfunction()
 
+function(_compile_model_and_add_to_target MODEL_FILE_IN TARGET COMPILER_FLAGS)
+  get_filename_component(SOURCE_FILE_PATH ${SHADER_FILE_IN} DIRECTORY)
+  get_filename_component(SOURCE_FILE_NAME ${SHADER_FILE_IN} NAME_WE)
+  set(OUTPUT_FILE_NAME ${SOURCE_FILE_NAME}.model)
+  set(OUTPUT_FULL_PATH ${CMAKE_CURRENT_BINARY_DIR}/${SOURCE_FILE_PATH}/${OUTPUT_FILE_NAME})
+  _compile_model(${CMAKE_CURRENT_SOURCE_DIR}/${SHADER_FILE_IN} ${OUTPUT_FULL_PATH} "${COMPILER_FLAGS}")
+  add_custom_target(${OUTPUT_FILE_NAME} DEPENDS ${OUTPUT_FULL_PATH})
+  add_dependencies(${TARGET} ${OUTPUT_FILE_NAME})
+endfunction()
+
+function(add_model)
+  cmake_parse_arguments(ADD_MODEL "" "SOURCE;TARGET" "" ${ARGN})
+  _compile_model_and_add_to_target(${ADD_MODEL_SOURCE} ${ADD_MODEL_TARGET})
+endfunction()
+
+function(add_models)
+  cmake_parse_arguments(ADD_MODELS "" "TARGET" "SOURCES" ${ARGN})
+  add_custom_target(${ADD_MODELS_TARGET})
+  foreach(FILE IN LISTS ${ADD_MODELS_SOURCES})
+    add_vulkan_fragment_shader(TARGET ${ADD_MODELS_TARGET} SOURCE ${FILE})
+  endforeach()
+endfunction()
--- cmake/shaders.cmake
+++ cmake/shaders.cmake
@@ -11,58 +11,77 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-function(_compile_shader_with_dxc TARGET_NAME SHADER_FILE ENTRY_POINT SHADER_TYPE FLAGS)
+function(_compile_shader_with_dxc SHADER_FILE_IN IL_FILE_OUT ENTRY_POINT SHADER_TYPE DXC_FLAGS)
+  add_custom_command(OUTPUT "${IL_FILE_OUT}"
+                    MAIN_DEPENDENCY ${SHADER_FILE_IN}
+                    COMMAND ${dxc_SOURCE_DIR}/bin/x64/dxc.exe -nologo -E${ENTRY_POINT} -T${SHADER_TYPE}_6_0 
+                                                              $<IF:$<CONFIG:DEBUG>,-Od,-O3> -Fo${IL_FILE_OUT}
+                                                              ${DXC_FLAGS}
+                                                              ${SHADER_FILE_IN}
+                    COMMENT "Building shader ${IL_FILE_OUT} from ${SHADER_FILE_IN}"
+                    VERBATIM)
 endfunction()
 
-macro(add_d3d12_shader TARGET_NAME SHADER_FILE ENTRY_POINT SHADER_TYPE)
-  get_filename_component(FILE_WE ${SHADER_FILE} NAME_WE)
-  get_filename_component(FILE_PATH ${SHADER_FILE} DIRECTORY)
-  file(MAKE_DIRECTORY "${FILE_PATH}")
+function(_compile_shader_and_add_to_target SHADER_FILE_IN TARGET OUTPUT_EXTENSION ENTRY_POINT SHADER_TYPE DXC_FLAGS)
+  get_filename_component(SOURCE_FILE_PATH ${SHADER_FILE_IN} DIRECTORY)
+  get_filename_component(SOURCE_FILE_NAME ${SHADER_FILE_IN} NAME_WE)
   string(TOLOWER ${ENTRY_POINT} ENTRY_POINT_LOWER)
-  set(HLSL_IN   ${PROJECT_SOURCE_DIR}/${SHADER_FILE})
-  set(DXIL_OUT ${FILE_PATH}/${FILE_WE}.${ENTRY_POINT_LOWER}.dxil)
-  add_custom_command(OUTPUT "${DXIL_OUT}"
-                     MAIN_DEPENDENCY ${HLSL_IN}
-                     COMMAND ${dxc_SOURCE_DIR}/bin/x64/dxc.exe -nologo -E${ENTRY_POINT} -T${SHADER_TYPE}_6_0 
-                                                               $<IF:$<CONFIG:DEBUG>,-Od,-O3> -Fo${DXIL_OUT}
-                                                               ${HLSL_IN}
-                     COMMENT "Building ${DXIL_OUT} from ${SHADER_FILE}"
-                     WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
-                     VERBATIM)
-  add_custom_target(${TARGET_NAME} DEPENDS "${DXIL_OUT}")
-endmacro()
+  set(OUTPUT_FILE_NAME ${SOURCE_FILE_NAME}.${ENTRY_POINT_LOWER}.${OUTPUT_EXTENSION})
+  set(OUTPUT_FULL_PATH ${CMAKE_CURRENT_BINARY_DIR}/${SOURCE_FILE_PATH}/${OUTPUT_FILE_NAME})
+  _compile_shader_with_dxc(${CMAKE_CURRENT_SOURCE_DIR}/${SHADER_FILE_IN} ${OUTPUT_FULL_PATH} ${ENTRY_POINT} ${SHADER_TYPE} "${DXC_FLAGS}")
+  add_custom_target(${OUTPUT_FILE_NAME} DEPENDS ${OUTPUT_FULL_PATH})
+  add_dependencies(${TARGET} ${OUTPUT_FILE_NAME})
+endfunction()
 
-macro(add_d3d12_vertex_shader TARGET_NAME SHADER_FILE ENTRY_POINT)
-  add_d3d12_shader(${TARGET_NAME} ${SHADER_FILE} ${ENTRY_POINT}  "vs")
-endmacro()
+function(add_vulkan_vertex_shader)
+  cmake_parse_arguments(ADD_SHADER "" "SOURCE;ENTRY_POINT;TARGET" "" ${ARGN})
+  _compile_shader_and_add_to_target(${ADD_SHADER_SOURCE} ${ADD_SHADER_TARGET} "spv" ${ADD_SHADER_ENTRY_POINT} "vs" "-spirv")
+endfunction()
 
-macro(add_d3d12_fragment_shader TARGET_NAME SHADER_FILE ENTRY_POINT)
-  add_d3d12_shader(${TARGET_NAME} ${SHADER_FILE} ${ENTRY_POINT}  "ps")
-endmacro()
+function(add_vulkan_fragment_shader)
+  cmake_parse_arguments(ADD_SHADER "" "SOURCE;ENTRY_POINT;TARGET" "" ${ARGN})
+  _compile_shader_and_add_to_target(${ADD_SHADER_SOURCE} ${ADD_SHADER_TARGET} "spv" ${ADD_SHADER_ENTRY_POINT} "ps" "-spirv")
+endfunction()
 
-macro(add_vulkan_shader TARGET_NAME SHADER_FILE ENTRY_POINT SHADER_TYPE)
-  get_filename_component(FILE_WE ${SHADER_FILE} NAME_WE)
-  get_filename_component(FILE_PATH ${SHADER_FILE} DIRECTORY)
-  file(MAKE_DIRECTORY "${FILE_PATH}")
-  string(TOLOWER ${ENTRY_POINT} ENTRY_POINT_LOWER)
-  set(HLSL_IN   ${PROJECT_SOURCE_DIR}/${SHADER_FILE})
-  set(SPIRV_OUT ${FILE_PATH}/${FILE_WE}.${ENTRY_POINT_LOWER}.spv)
-  add_custom_command(OUTPUT "${SPIRV_OUT}"
-                     MAIN_DEPENDENCY ${HLSL_IN}
-                     COMMAND ${dxc_SOURCE_DIR}/bin/x64/dxc.exe -spirv -nologo -E${ENTRY_POINT} -T${SHADER_TYPE}_6_0 
-                                                               $<IF:$<CONFIG:DEBUG>,-Od,-O3> -Fo${SPIRV_OUT}
-                                                               ${HLSL_IN}
-                     COMMENT "Building ${SPIRV_OUT} from ${SHADER_FILE}"
-                     WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
-                     VERBATIM)
-  add_custom_target(${TARGET_NAME} DEPENDS "${SPIRV_OUT}")
-endmacro()
+function(add_vulkan_vertex_shaders)
+  cmake_parse_arguments(ADD_SHADERS "" "TARGET" "SOURCES" ${ARGN})
+  add_custom_target(${ADD_SHADERS_TARGET})
+  foreach(FILE IN LISTS ${ADD_SHADERS_SOURCES})
+    add_vulkan_fragment_shader(TARGET ${ADD_SHADERS_TARGET} SOURCE ${FILE} ENTRY_POINT "VSMain")
+  endforeach()
+endfunction()
 
-macro(add_vulkan_vertex_shader TARGET_NAME SHADER_FILE ENTRY_POINT)
-  add_vulkan_shader(${TARGET_NAME} ${SHADER_FILE} ${ENTRY_POINT}  "vs")
-endmacro()
+function(add_vulkan_fragment_shaders)
+  cmake_parse_arguments(ADD_SHADERS "" "TARGET" "SOURCES" ${ARGN})
+  add_custom_target(${ADD_SHADERS_TARGET})
+  foreach(FILE IN LISTS ${ADD_SHADERS_SOURCES})
+    add_vulkan_fragment_shader(TARGET ${ADD_SHADERS_TARGET} SOURCE ${FILE} ENTRY_POINT "PSMain")
+  endforeach()
+endfunction()
 
-macro(add_vulkan_fragment_shader TARGET_NAME SHADER_FILE ENTRY_POINT)
-  add_vulkan_shader(${TARGET_NAME} ${SHADER_FILE} ${ENTRY_POINT}  "ps")
-endmacro()
+function(add_d3d12_vertex_shader)
+  cmake_parse_arguments(ADD_SHADER "" "SOURCE;ENTRY_POINT;TARGET" "" ${ARGN})
+  _compile_shader_and_add_to_target(${ADD_SHADER_SOURCE} ${ADD_SHADER_TARGET} "dxil" ${ADD_SHADER_ENTRY_POINT} "vs" "")
+endfunction()
+
+function(add_d3d12_fragment_shader)
+  cmake_parse_arguments(ADD_SHADER "" "SOURCE;ENTRY_POINT;TARGET" "" ${ARGN})
+  _compile_shader_and_add_to_target(${ADD_SHADER_SOURCE} ${ADD_SHADER_TARGET} "dxil" ${ADD_SHADER_ENTRY_POINT} "ps" "")
+endfunction()
+
+function(add_d3d12_vertex_shaders)
+  cmake_parse_arguments(ADD_SHADERS "" "TARGET" "SOURCES" ${ARGN})
+  add_custom_target(${ADD_SHADERS_TARGET})
+  foreach(FILE IN LISTS ${ADD_SHADERS_SOURCES})
+    add_vulkan_fragment_shader(TARGET ${ADD_SHADERS_TARGET} SOURCE ${FILE} ENTRY_POINT "VSMain")
+  endforeach()
+endfunction()
+
+function(add_d3d12_fragment_shaders)
+  cmake_parse_arguments(ADD_SHADERS "" "TARGET" "SOURCES" ${ARGN})
+  add_custom_target(${ADD_SHADERS_TARGET})
+  foreach(FILE IN LISTS ${ADD_SHADERS_SOURCES})
+    add_vulkan_fragment_shader(TARGET ${ADD_SHADERS_TARGET} SOURCE ${FILE} ENTRY_POINT "PSMain")
+  endforeach()
+endfunction()
 
