4f8383005648cd9883688cdb4e6a7b1e22956d33
Chris Glover <chrisglover@google.com>
2022-10-31 09:33:08 +0000

    Fixup dxc for linux build

---
+++ cmake/dxc.cmake
@@ -0,0 +1,38 @@
+# Copyright 2022 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# If building for Windows
+if(WIN32)
+    FetchContent_Declare(
+        dxc
+        URL https://github.com/microsoft/DirectXShaderCompiler/releases/download/v1.7.2207/dxc_2022_07_18.zip
+        URL_HASH MD5=e226cd9fee7f3b9015c0767c9035123c
+    )
+    FetchContent_MakeAvailable(dxc)
+    FetchContent_GetProperties(dxc SOURCE_DIR dxc_SOURCE_DIR)
+    add_library(dxc SHARED IMPORTED)
+    set_target_properties(
+                dxc PROPERTIES
+                IMPORTED_LOCATION "${dxc_SOURCE_DIR}/bin/x64/dxcompiler.dll;${dxc_SOURCE_DIR}/bin/x64/dxil.dll"
+                IMPORTED_IMPLIB "${dxc_SOURCE_DIR}/lib/x64/dxcompiler.lib"
+                INTERFACE_INCLUDE_DIRECTORIES "${dxc_SOURCE_DIR}/inc")
+                else()
+endif()
+    
+# If building from Windows.
+if(CMAKE_HOST_SYSTEM_NAME EQUAL Windows)
+  set(DXC_EXECUTABLE ${dxc_SOURCE_DIR}/bin/x64/dxc.exe)
+else()
+
+endif()
